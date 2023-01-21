758b582acb6a6e23982f17f5aa6e756323d50415
João Pedro <jprodriguesdasilva19@gmail.com>
2023-01-21 12:19:24 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// colocar na variavel o formulário
+const form = document.querySelector("#form-habits")
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
+
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  //dia atual do pc
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("dia já incluso ❌")
+    return
+  }
+
+  alert("Adicionado com sucesso ✔")
+  nlwSetup.addDay(today)
+}
+
+function save() {
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+}
+
+// const data = {
+//   run: ["01-01", "01-02", "01-06", "01-07", "01-08"],
+//   takePills: ["01-03"],
+//   journal: ["01-02"],
+// }
+
+//localStorage pra salvar infos na memoria
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
+nlwSetup.setData(data)
+nlwSetup.load()
